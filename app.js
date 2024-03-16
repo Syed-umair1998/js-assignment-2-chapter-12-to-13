22a6900a8e447c9e64ef375a5beab338801944aa
Syed-umair1998 <160543227+Syed-umair1998@users.noreply.github.com>
2024-03-16 14:37:29 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,66 @@
+// question # 1 
+// var x=prompt("enter name");
+// var y=x.toUpperCase();
+// if (x==y) {
+//     alert ("uppercase");}
+   
+ 
+// question # 3
+   // var number = (prompt("Enter a number:"));
+
+    // // Checking if the number is positive, negative, or zero
+    // if (number > 0) {
+    //     console.log("The number is positive.");
+    // } else if (number < 0) {
+    //     console.log("The number is negative.");
+    // } else {
+    //     console.log("The number is zero.");
+    // }
+
+    // question # 4.
+// function isVowel(character) {
+//     // Converting the character to lowercase for case-insensitivity
+//     character = character.toLowerCase();
+
+//     // Checking if the character is a vowel
+//     if (character === 'a' || character === 'e' || character === 'i' || character === 'o' || character === 'u') {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// // Example usage:
+// var inputChar = prompt("Enter a character:");
+// if (inputChar.length === 1) {
+//     var result = isVowel(inputChar);
+//     console.log("Is the character a vowel? " + result);
+// } else {
+//     console.log("Please enter only one character.");
+// }
+
+// Question # 5
+// Ask the user to enter their password
+// var enteredPassword = prompt("Enter your password:");
+
+// var correctPassword = ("mySecretPassword");
+
+// if (!enteredPassword) {
+//     console.log("Please enter your password.");
+// } else if (enteredPassword === correctPassword) {
+//     // Check if both passwords are the same
+//     console.log("Correct! The password you entered matches the original password.");
+// } else {
+//     console.log("Incorrect password.");
+// }
+
+// QUESTION # 6.
+// var hour = prompt("enter hour");
+
+// if (hour <18){
+//     console.log("Good day")
+// }
+// else {
+//     console.log("good evening")
+// }
+
